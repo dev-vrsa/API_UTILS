bbd96911b60e1a902d9815f9481eccbc173b207e
victorsa <victoredo@gmail.com>
2025-03-17 03:00:04 -0300

    validations

--- src/main/java/com/app/utility/me/router/CepRouter.java
+++ src/main/java/com/app/utility/me/router/CepRouter.java
@@ -1,7 +1,10 @@
 package com.app.utility.me.router;
 
 import com.app.utility.me.core.usecase.GetCepInfo;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
+import lombok.NonNull;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,7 +17,7 @@ public class CepRouter {
     private final GetCepInfo getCepInfo;
 
     @GetMapping("/cep")
-    public ResponseEntity<GetCepInfo.CepInfo> getCepInfo(@RequestParam("cep") String cep) {
+    public ResponseEntity<GetCepInfo.CepInfo> getCepInfo(@Valid @NonNull @NotEmpty @RequestParam("cep") String cep) {
 
             return ResponseEntity.ok(getCepInfo.apply(cep));
     }
